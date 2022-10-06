a3fdda35273cfde80f42a49998a2c54643d45947
Jeffrey-Ramirez <jeffreycap12@gmail.com>
2022-10-06 00:59:05 -0600

    Ecomerce:

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+// Models
+const { User } = require('../models/user.model');
+
+// Utils
+const { catchAsync } = require('../utils/catchAsync.util');
+const { AppError } = require('../utils/appError.util');
+
+const protectSession = catchAsync(async (req, res, next) => {
+  let token;
+
+  // Extract token from headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // ['Bearer', 'token']
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('Session invalid', 403));
+  }
+
+  // Validate token
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+  const user = await User.findOne({
+    where: { id: decoded.id, status: 'active' },
+  });
+
+  if (!user) {
+    return next(
+      new AppError('The owner of this token is no longer available', 403)
+    );
+  }
+
+  req.sessionUser = user;
+  next();
+});
+
+const protectAdmin = catchAsync(async (req, res, next) => {
+  if (req.sessionUser.role !== 'admin') {
+    return next(new AppError('Access not granted', 403));
+  }
+
+  next();
+});
+
+module.exports = {
+  protectSession,
+  protectAdmin,
+};
